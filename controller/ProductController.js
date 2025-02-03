526e357bebf5b3fbdf1c09f93621bccb3f1a216c
sughashini <114178516+sughashini@users.noreply.github.com>
2025-02-03 11:59:42 +0530

    Update ProductController.js

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -23,5 +23,53 @@ exports.getSingleProduct = async (req, res) =>{
         res.status(500).json({msg : err.message})
     }
 }
+exports.addNewProduct = async (req, res)=>{
+    try{
+        const newProduct = req.body;
+        const check = await Product.findOne({proId:newProduct.proId})
+        if(!check){            
+            await Product.create(req.body);
+            res.json(newProduct)
+        }
+        else
+            return res.status(500).json({msg:'Record already exixts'})     
+    }
+    catch(err){
+        res.status(500).json({msg:err.message})
+    }
+
+}
+
+exports.updateExistProduct = async (req, res)=>{
+    try{
+        const product = req.body;
+        const check = await Product.findOne({proId:product.proId})
+        if(check){
+            await Product.updateOne(product)
+            res.json(product)
+        }
+        else
+            res.status(404).json({msg: 'Data doesnt exist'})
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+exports.deleteProduct = async (req, res) =>{
+    try{
+        const check = await Product.findOne({proId:req.params.id})
+        if(check){
+            await Product.deleteOne({proId: check.proId})
+            res.json(check)
+        }
+        else
+            res.status(404).json({msg: 'Data doesnt exist'})
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
 
 
