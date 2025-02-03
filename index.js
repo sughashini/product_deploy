2efc5ef9db9fb22e71af51b43083b9ebb85d11f7
sughashini <114178516+sughashini@users.noreply.github.com>
2025-02-03 11:35:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 
 app.listen('5000', ()=> console.log('server running on 5000'))
 
-mongoose.connect('mongodb://localhost:27017/sample')
+mongoose.connect('mongodb+srv://sughashini30:<db_password>@mycluster.vytpl.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster')
     .then(()=> console.log('database connected..'))
     .catch((err) => console.log(err))
 
